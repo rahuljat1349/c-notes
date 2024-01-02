7a74c1699f16be5715993ee637d316032c1b2a37
Rahul Jat <rahuldudi1349@gmail.com>
2024-01-02 21:52:11 +0530

    reading comments

---
+++ 02_Data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int age = 20;
+
+    float pi = 3.04;
+
+    char hashtag = '#';
+
+    // there is no boolian and strings
+
+    return  0;
+}
---
+++ 03_Constants.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    // Integer constants => '1', '24', '-1'
+
+    // Real constants => '1.0', '-2.4', '3.14'
+
+    // Character constants => '#', '@', 'a', 'A'
+
+    return 0;
+}
