f8d05d6fc8e756942abfc3f0e8b319992d1e52db
Rahul Dudi <rahuldudi1349@gmail.com>
2024-01-29 20:50:25 +0530

    completed conditional statements

--- 07_conditional_statements.c
+++ 07_conditional_statements.c
@@ -8,7 +8,21 @@ int main(){
 
 //  switch case
 
+char ch;
+printf("enter a character : ");
+scanf("%c",&ch);
+if (ch >= 'A' && ch <= 'Z')
+{
+    printf("upper case \n");
+}
+else if (ch >= 'a' && ch <= 'z')
+{
+    printf("lower case \n");
+}
+else
+{
+    printf("not an english letter \n");
+};
 
-// 2:20:00
 
 };
