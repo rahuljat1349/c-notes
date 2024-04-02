c540e4afd90dc110805a37a9ec95e7ccc1736dff
Rahul Dudi <rahuldudi1349@gmail.com>
2024-04-02 22:55:33 +0530

    add loops

---
+++ 08_loops.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+
+// for (int i = 0; i < 5; i++)
+// {
+//     printf("Hello world \n");
+// }
+
+// int i = 0;
+// while (i < 5)
+// {
+//    printf("Hello world \n");
+//    i++;
+// }
+
+
+};
