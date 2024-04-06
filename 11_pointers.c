f9b4226a8fc242372ad5c30fac3dc255728379d9
Rahul Dudi <rahuldudi1349@gmail.com>
2024-04-06 22:54:44 +0530

    add pinters

---
+++ 11_pointers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    int age = 22;
+    int *ptr = &age;
+    int _age = *ptr;
+
+    printf("%d \n",_age);
+
+    printf("%p",&age);
+
+
+    return 0;
+
+}
+
