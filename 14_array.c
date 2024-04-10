1b6e0f8dd8d6dc9c65128f27fbe75666eefbe0bb
Rahul Dudi <rahuldudi1349@gmail.com>
2024-04-10 23:06:47 +0530

    add arrays

---
+++ 14_array.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+void reverse(int arr[],int n);
+void printArray(int arr[],int n);
+// Array is a pinter
+int main(){
+
+
+// // printing array values with pointers
+// int arr[] = {2,5,9};
+
+// // int *ptr = &arr[0];
+// // or
+
+// int *ptr = arr;
+
+// for (int i = 0; i < 3; i++)
+// {
+//    printf("%d \n",*(ptr+i));
+
+// }
+
+
+// Array as a function argument
+
+
+    
+// void printnumbers(int arr[],int n)   (first is array and second is length of array)
+// or 
+// void printnumbers(int *arr,int n)
+
+
+
+// // multidimensional arrays
+
+// int arr[][] = {{1,2},{3,4}};
+
+// access
+// int[0][1]
+
+
+// Reverse an array
+int arr[] = {1,2,3,4,5,6};
+ reverse(arr,6);
+ printArray(arr,6);
+
+
+
+    return 0;
+ }
+
+ void reverse(int arr[],int n){
+
+    for (int i = 0; i < n/2; i++)
+    {
+       int firstValue = arr[i];
+       int secondValue = arr[n-i-1];
+
+       arr[i] = secondValue;
+       arr[n-i-1] = firstValue; 
+    }
+    
+
+ }
+ void printArray(int arr[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d \t",arr[i]);
+    }
+    printf("\n");
+ }
